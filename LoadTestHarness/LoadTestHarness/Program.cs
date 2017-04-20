ff5312a2cb5332e94868e28e96b18a8cd814c6f3
jack ni <jack.ni@scopeglobal.com>
2017-04-20 23:15:09 +0930

    init commit

---
+++ LoadTestHarness/LoadTestHarness/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadTestHarness
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running loadtest. Press enter key to exit...");
+
+            int startNumberOfClients = Convert.ToInt16(ConfigurationManager.AppSettings["StartNumberOfClient"]);
+            int maxNumberOfClients = Convert.ToInt16(ConfigurationManager.AppSettings["MaxNumberOfClient"]);
+            int stepNumberOfClients = Convert.ToInt16(ConfigurationManager.AppSettings["StepNumberOfClients"]);
+            int stepTime = Convert.ToInt16(ConfigurationManager.AppSettings["StepTime"]); ;
+            int messageInterval = Convert.ToInt16(ConfigurationManager.AppSettings["MessageInterval"]);
+            int runDuration = Convert.ToInt16(ConfigurationManager.AppSettings["TestDurationAsSec"]);
+            string baseUrl = ConfigurationManager.AppSettings["HubUrl"].ToString();
+            string hubName = ConfigurationManager.AppSettings["HubName"].ToString();
+            string serverCallback = ConfigurationManager.AppSettings["ServerSideCallBack"].ToString();
+
+            string[] testPayload = new string[] { "key:key1, value:value1", "key:key2, value:value2" };
+
+            ConnectionManager instance = new ConnectionManager();
+            instance.Init(baseUrl, hubName, serverCallback, null);
+
+            instance.GenerateLoad(startNumberOfClients, maxNumberOfClients, stepNumberOfClients,
+                stepTime, messageInterval, runDuration);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LoadTestHarness/LoadTestHarness/SignalRHubConnection.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LoadTestHarness
+{
+    internal class SignalRHubConnection
+    {
+        private IHubProxy proxy = null;
+        private HubConnection connection = null;
+
+        private string url = string.Empty;
+        private string hubName = string.Empty;
+        private string callbackMethodName = string.Empty;
+        private Action<dynamic> callbackMethodAction;
+        private ConnectionManager connectionManagerInstance;
+
+        internal SignalRHubConnection(ConnectionManager instance, string url, string hubName)
+        {
+            this.connectionManagerInstance = instance;
+            this.url = url;
+            this.hubName = hubName;
+            Initialize();
+        }
+
+        internal SignalRHubConnection(ConnectionManager instance, string url, string hubName,
+            string callbackMethodName, Action<dynamic> callbackMethodAction)
+        {
+            this.connectionManagerInstance = instance;
+            this.url = url;
+            this.hubName = hubName;
+            this.callbackMethodName = callbackMethodName;
+            this.callbackMethodAction = callbackMethodAction;
+            Initialize();
+        }
+
+        internal string ConnectionId
+        {
+            get { return connection.ConnectionId; }
+        }
+
+        internal void Invoke(string hubMethodName, params object[] hubMethodParams)
+        {
+            CallHubMethod(hubMethodName, hubMethodParams);
+        }
+
+        internal async void Invoke(string hubMethodName, int interval, params object[] hubMethodParams)
+        {
+            while (!connectionManagerInstance.IsStopped)
+            {
+                CallHubMethod(hubMethodName, hubMethodParams);
+
+                Debug.WriteLine("Wait for {0} seconds before invoking again on Connection : {1} (2)",
+                    interval, ConnectionId);
+                await Task.Delay(interval * 1000);
+            }
+        }
+
+        private void CallHubMethod(string hubMethodName, params object[] hubMethodParams)
+        {
+            try
+            {
+                if (!connectionManagerInstance.IsStopped)
+                {
+                    if (this.connection.State == ConnectionState.Connected)
+                    {
+                        Debug.WriteLine("Invoking '{0}' on Connection : {1} ({2}) at {3}", hubMethodName,
+                            this.ConnectionId, Thread.CurrentThread.GetHashCode(), DateTime.Now);
+                        proxy.Invoke(hubMethodName, hubMethodParams);
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+                Debug.WriteLine("Exception '{0}' occured on Connection : {1}", exc.Message, ConnectionId);
+            }
+        }
+
+        private void Initialize()
+        {
+            connection = new HubConnection(url, true);
+            proxy = connection.CreateHubProxy(hubName);
+            if (callbackMethodName != string.Empty && callbackMethodAction != null)
+                proxy.On(callbackMethodName, x => callbackMethodAction(x));
+            connection.Start().Wait();
+        }
+    }
+}
